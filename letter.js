1ad8d50496bb098b50e19514a272161308824ad6
HaifaSeyam <h.abuamro@hotmail.com>
2019-02-21 17:12:20 -0600

    letter.js has been created

---
+++ letter.js
@@ -0,0 +1,22 @@
+
+function Letter(char) {
+    this.char = char;
+    this.guessed = false;
+
+    this.displayGuessed = function() {
+        if (this.guessed) {
+            return this.char;
+        } else {
+            return ("_");
+        }
+    };
+    this.checkGuessed = function (guessedChar) {
+        if (guessedChar === this.char) {
+            this.guessed = true;
+        } else {
+            this.guessed = false;
+        }
+    }
+}
+
+module.exports = Letter;

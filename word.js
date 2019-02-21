08760f7e9f5e5eb89d4a501921201c57b6192c82
HaifaSeyam <h.abuamro@hotmail.com>
2019-02-21 17:24:56 -0600

    displaying the guessed letters issue has been fixed

--- word.js
+++ word.js
@@ -2,20 +2,25 @@ var Letter = require("./letter");
 
 function Word(word) {
     this.lettersArr = word.split("").map(letter => {return new Letter(letter)});
+    console.log(this.lettersArr);
 
     this.displayGuesses = function () {
         var guesses = "";
-        for (var i = 0 ; i < this.lettersArr.length ; i++){
-            guesses += this.lettersArr[i].displayGuessed();
-        }
-
+        this.lettersArr.forEach(function(letter) {
+            guesses += letter.displayGuessed() + " ";
+        });
+        
         console.log(guesses);
-    };
-    this.updateGuessedLetterArr = function (inputChar) {
-        for (var i = 0 ; i < this.lettersArr.length ; i++) {
-            this.lettersArr[i].checkGuessed(inputChar);
-        }
     }
+
+    this.updateGuessedLetterArr = function(inputChar){
+        this.lettersArr.forEach(function(letter){
+            if(letter.guessed === false && letter.checkGuessed(inputChar) === true){
+                letter.guessed = true;
+            }
+        });
+}
+
 }
 
 module.exports = Word;
